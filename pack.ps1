4e5ef2d80636a4a7e02539e67908cc00dd61eee8
Andrey Eliseev <eliseevandrey2005@gmail.com>
2024-04-21 02:38:45 +0700

    Rewrote the build fiels again. Moved all of the build logic into the main .csproj file.

--- pack.ps1
+++
@@ -1,4 +0,0 @@
-cd ./Build
-dotnet build ../Facepunch.Steamworks/Facepunch.Steamworks.csproj -p:TargetOS=windows -o ../bin/windows
-dotnet build ../Facepunch.Steamworks/Facepunch.Steamworks.csproj -p:TargetOS=unix -o ../bin/unix
-nuget pack ./Facepunch.Steamworks.nuspec -OutputDirectory ../bin
